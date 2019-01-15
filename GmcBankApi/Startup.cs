53ac2a1aa13d8f10df3fafd957d8e4aaef1eba37
Achour <achourmedhedi@gmail.com>
2019-01-15 22:48:48 +0100

    swagger

--- GmcBankApi/Startup.cs
+++ GmcBankApi/Startup.cs
@@ -5,7 +5,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace GmcBankApi
 {
@@ -24,6 +27,36 @@ namespace GmcBankApi
             services
              .AddScoped<IBank<Client<AbsctractAccount<Transaction>, Transaction>, AbsctractAccount<Transaction>, Transaction> , Bank<Client<AbsctractAccount<Transaction>, Transaction>, AbsctractAccount<Transaction>, Transaction>>();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Dhabout Lguerda API",
+                    Description = "Wadh3iyet jane7 el ta2ira \\_",
+                    TermsOfService = "Nik ala 3ajla bil volant",
+                    Contact = new Contact
+                    {
+                        Name = "Taxiste",
+                        Email = string.Empty,
+                        Url = "http://achour.69"
+                    },
+                    License = new License
+                    {
+                        Name = "Ja3bou ma7loul",
+                        Url = "https://sami-forja.xxx/permis"
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+
+            });
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -40,6 +73,18 @@ namespace GmcBankApi
 
 
             app.UseAuthentication();
+
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dhabout Lguerda API V1");
+            });
+
 
             app.UseMvc(routes =>
             {
